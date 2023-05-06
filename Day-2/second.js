b088f0f4902f96c6f1d88b2d0cbd05eac644386a
Jeevan Joseph <jeevanjoseph1310@gmail.com>
2023-05-06 20:56:59 +0530

    added  a second day programs

---
+++ Day-2/second.js
@@ -0,0 +1,9 @@
+// Accept input from the user
+let number1 = prompt("Enter the first number:");
+let number2 = prompt("Enter the second number:");
+
+// Convert the input to numbers and add them
+let sum = Number(number1) + Number(number2);
+
+// Display the result
+alert("The sum of the two numbers is: " + sum);
