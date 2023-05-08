96f964f0d9717f4deeb990e5dc168bf816956ad3
Jeevan Joseph <jeevanjoseph1310@gmail.com>
2023-05-08 20:28:03 +0530

    finalised program for day-4

--- Day-4/fouth.js
+++ Day-4/fouth.js
@@ -4,6 +4,8 @@ const form = document.querySelector('form');
 const todoInput = document.querySelector('#todo-input');
 const todoListElement = document.querySelector('#todo-list');
 
+
+//We need to create function to add the todo items to the todolist and then new element is added it will display on the li element
 form.addEventListener('submit', function(event) {
   event.preventDefault();
   const todoItem = todoInput.value;
@@ -15,6 +17,8 @@ form.addEventListener('submit', function(event) {
     todoInput.value = '';
   }
 });
+
+// also we need to add option to delete the todo list by clickimng over it and it will deleted from the list
 todoListElement.addEventListener('click', function(event) {
     const clickedListItem = event.target;
     const clickedListItemIndex = Array.from(todoListElement.children).indexOf(clickedListItem);
