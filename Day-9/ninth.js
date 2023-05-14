b4be020b079b471b144837bcf6a289e8ccd50aa7
Jeevan Joseph <jeevanjoseph1310@gmail.com>
2023-05-14 18:48:51 +0530

    recoded entire website

--- Day-9/ninth.js
+++ Day-9/ninth.js
@@ -2,46 +2,52 @@ let timerInterval;
 let milliseconds = 0;
 let seconds = 0;
 let minutes = 0;
+let isRunning = false;
 
 function start() {
+  if (!isRunning) {
     timerInterval = setInterval(updateTime, 10); // Update every 10 milliseconds
+    isRunning = true;
+  }
 }
 
 function stop() {
-    clearInterval(timerInterval);
+  clearInterval(timerInterval);
+  isRunning = false;
 }
 
 function reset() {
-    clearInterval(timerInterval);
-    milliseconds = 0;
-    seconds = 0;
-    minutes = 0;
-    updateDisplay();
+  clearInterval(timerInterval);
+  milliseconds = 0;
+  seconds = 0;
+  minutes = 0;
+  updateDisplay();
+  isRunning = false;
 }
 
 function updateTime() {
-    milliseconds += 10;
+  milliseconds += 10;
 
-    if (milliseconds === 1000) {
+  if (milliseconds === 1000) {
     milliseconds = 0;
     seconds++;
-    }
+  }
 
-    if (seconds === 60) {
+  if (seconds === 60) {
     seconds = 0;
     minutes++;
-    }
+  }
 
-    updateDisplay();
+  updateDisplay();
 }
 
 function updateDisplay() {
-    const displayElement = document.getElementById("display");
-    displayElement.textContent = `${padTime(minutes)}:${padTime(
+  const displayElement = document.getElementById("display");
+  displayElement.textContent = `${padTime(minutes)}:${padTime(
     seconds
-    )}:${padTime(milliseconds / 10)}`;
+  )}:${padTime(milliseconds / 10)}`;
 }
 
 function padTime(time) {
-    return time.toString().padStart(2, "0");
+  return time.toString().padStart(2, "0");
 }
