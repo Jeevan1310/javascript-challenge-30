cb20d2556099b34ede71c921ad990cc78a179786
Jeevan Joseph <jeevanjoseph1310@gmail.com>
2023-05-15 20:44:50 +0530

    started for Day -10

---
+++ Day-10/tenth.js
@@ -0,0 +1,8 @@
+if (/Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor)) {
+    // Code to run if the browser is Google Chrome
+    console.log("You are using Google Chrome!");
+  } else {
+    // Code to run if the browser is not Google Chrome
+    console.log("You are not using Google Chrome.");
+  }
+// basically nothing for this website so i added a user agent checker  
