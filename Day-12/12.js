da2ed5a5693769e979dd50c00f928ae41727e889
Jeevan Joseph <jeevanjoseph1310@gmail.com>
2023-05-17 22:02:30 +0530

    added a fix

--- Day-12/12.js
+++ Day-12/12.js
@@ -1,32 +1,41 @@
-const slider = document.querySelector('.slider');
-let currentSlide = 0;
-const slides = slider.querySelectorAll('img');
-const totalSlides = slides.length;
-
-function showSlide(index) {
-  if (index < 0 || index >= totalSlides) return;
+document.addEventListener('DOMContentLoaded', function() {
+    const slider = document.querySelector('.slider');
+    const slides = slider.getElementsByTagName('img');
+    let currentSlide = 0;
+    const totalSlides = slides.length;
   
-  slides.forEach((slide, i) => {
-    slide.style.transform = `translateX(${(i - index) * 100}%)`;
+    function showSlide(index) {
+      if (index < 0 || index >= totalSlides) return;
+  
+      for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+      }
+      
+      slides[index].style.display = 'block';
+      currentSlide = index;
+    }
+  
+    function nextSlide() {
+      const nextIndex = (currentSlide + 1) % totalSlides;
+      showSlide(nextIndex);
+    }
+  
+    function prevSlide() {
+      const prevIndex = (currentSlide - 1 + totalSlides) % totalSlides;
+      showSlide(prevIndex);
+    }
+  
+    // Optional: Auto-play the slider
+    const autoplayInterval = setInterval(nextSlide, 3000);
+  
+    // Usage example:
+    // Show the next slide when clicking a "Next" button
+    document.getElementById('nextBtn').addEventListener('click', nextSlide);
+  
+    // Show the previous slide when clicking a "Previous" button
+    document.getElementById('prevBtn').addEventListener('click', prevSlide);
+  
+    // Show the initial slide
+    showSlide(currentSlide);
   });
   
-  currentSlide = index;
-}
-
-function nextSlide() {
-  showSlide(currentSlide + 1);
-}
-
-function prevSlide() {
-  showSlide(currentSlide - 1);
-}
-
-
-const autoplayInterval = setInterval(nextSlide, 3000);
-
-
-
-document.getElementById('nextBtn').addEventListener('click', nextSlide);
-
-
-document.getElementById('prevBtn').addEventListener('click', prevSlide);
