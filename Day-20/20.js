b41a74a0806b1d7d16320ff940a6007c0de812e0
Jeevan Joseph <jeevanjoseph1310@gmail.com>
2023-05-27 21:56:58 +0530

    added the program for day-21

--- Day-20/20.js
+++ Day-20/20.js
@@ -1,15 +1,49 @@
-function calculateFactorial() {
-    var numberInput = document.getElementById("number");
-    var resultDiv = document.getElementById("result");
-    var number = parseInt(numberInput.value);
+document.addEventListener('DOMContentLoaded', () => {
+  const canvas = document.getElementById('canvas');
+  const context = canvas.getContext('2d');
+  let isDrawing = false;
 
-    if (isNaN(number) || number < 0) {
-      resultDiv.innerHTML = "Please enter a non-negative number.";
-    } else {
-      var factorial = 1;
-      for (var i = 2; i <= number; i++) {
-        factorial *= i;
-      }
-      resultDiv.innerHTML = "Factorial of " + number + " is: " + factorial;
-    }
+  canvas.addEventListener('mousedown', startDrawing);
+  canvas.addEventListener('mousemove', draw);
+  canvas.addEventListener('mouseup', stopDrawing);
+  canvas.addEventListener('mouseout', stopDrawing);
+
+  const resetBtn = document.getElementById('resetBtn');
+  resetBtn.addEventListener('click', resetCanvas);
+
+  const saveBtn = document.getElementById('saveBtn');
+  saveBtn.addEventListener('click', saveCanvas);
+
+  function startDrawing(e) {
+      isDrawing = true;
+      draw(e);
   }
+
+  function draw(e) {
+      if (!isDrawing) return;
+
+      const rect = canvas.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+
+      context.lineTo(x, y);
+      context.stroke();
+  }
+
+  function stopDrawing() {
+      isDrawing = false;
+      context.beginPath();
+  }
+
+  function resetCanvas() {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
+  function saveCanvas() {
+      const image = canvas.toDataURL();
+      const link = document.createElement('a');
+      link.href = image;
+      link.download = 'drawing.png';
+      link.click();
+  }
+});
